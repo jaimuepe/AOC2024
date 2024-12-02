78e66a63bae8b9f5bfd805762668246d7a0ff10f
jaimuepe <jaimuepe@gmail.com>
2024-12-02 18:04:38 +0100

    day 02

--- AOC2024/Day02.cs
+++ AOC2024/Day02.cs
@@ -6,15 +6,74 @@ public class Day02(int year) : AOC_DayBase(year, 2)
 {
     protected override void SolveA_Internal(string input)
     {
-        AOC_Logger.Display("Solving A!");
+        var reports = AOC_Utils.ParseAsIntMatrix(input);
+
+        var safeReports = reports
+            .Count(report => IsSafeReport(report));
+
+        AOC_Logger.Display("Safe reports: " + safeReports);
     }
 
     protected override void SolveB_Internal(string input)
     {
-        AOC_Logger.Display("Solving B!");
+        var reports = AOC_Utils.ParseAsIntMatrix(input);
+
+        var safeReports = 0;
+
+        foreach (var report in reports)
+        {
+            var isSafe = IsSafeReport(report);
+
+            if (!isSafe)
+            {
+                // create sub-reports removing a different item each time
+                for (var i = 0; i < report.Count; i++)
+                {
+                    var subReport = AOC_Utils.RemoveOneItem(report, i);
+
+                    isSafe = IsSafeReport(subReport);
+                    if (isSafe) break;
+                }
+            }
+
+            if (isSafe) safeReports++;
+        }
+
+        AOC_Logger.Display("Safe reports: " + safeReports);
     }
 
     protected override string TestInput => """
-    
+                                           7 6 4 2 1
+                                           1 2 7 8 9
+                                           9 7 6 2 1
+                                           1 3 2 4 5
+                                           8 6 4 4 1
+                                           1 3 6 7 9
                                            """;
+
+    private static bool IsSafeReport(List<int> report)
+    {
+        const int minDistance = 1;
+        const int maxDistance = 3;
+
+        var lastIncrement = 0;
+
+        for (var i = 1; i < report.Count; i++)
+        {
+            var increment = report[i] - report[i - 1];
+
+            var absIncrement = Math.Abs(increment);
+
+            // too far!
+            if (absIncrement < minDistance || absIncrement > maxDistance) return false;
+
+            // change of direction!
+            if (lastIncrement != 0 &&
+                Math.Sign(increment) != Math.Sign(lastIncrement)) return false;
+
+            lastIncrement = increment;
+        }
+
+        return true;
+    }
 }
--- AOCHelper/AOC_Utils.cs
+++ AOCHelper/AOC_Utils.cs
@@ -5,11 +5,50 @@ public static class AOC_Utils
     public static IEnumerable<string> SplitLines(string input)
     {
         using var reader = new StringReader(input);
-        
+
         string? line;
         while ((line = reader.ReadLine()) != null)
         {
             yield return line;
         }
+    }
+
+    public static List<T> RemoveOneItem<T>(List<T> list, int index)
+    {
+        // https://stackoverflow.com/a/41958908
+        
+        var listCount = list.Count;
+
+        // Create an array to store the data.
+        var result = new T[listCount - 1];
+
+        // Copy element before the index.
+        list.CopyTo(0, result, 0, index);
+
+        // Copy element after the index.
+        list.CopyTo(index + 1, result, index, listCount - 1 - index);
+
+        return [..result];
+    }
+
+    public static List<List<int>> ParseAsIntMatrix(string input, string separator = " ")
+    {
+        var outerList = new List<List<int>>();
+
+        foreach (var line in SplitLines(input))
+        {
+            var tokens = line.Split(separator);
+
+            var innerList = new List<int>();
+
+            foreach (var tok in tokens)
+            {
+                innerList.Add(int.Parse(tok));
+            }
+
+            outerList.Add(innerList);
+        }
+
+        return outerList;
     }
 }
